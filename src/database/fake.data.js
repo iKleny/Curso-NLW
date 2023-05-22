b03076cebc291c59d05a55161a45bfef07709c01
Daniel Rainer <134097809+iKleny@users.noreply.github.com>
2023-05-22 14:20:53 -0300

    Update fake.data.js

--- src/database/fake.data.js
+++ src/database/fake.data.js
@@ -15,7 +15,7 @@ const proffys = [{
     name: "Matheus rugal", 
     avatar: "https://pps.whatsapp.net/v/t61.24694-24/160754171_340630651121711_793089584898896718_n.jpg?ccb=11-4&oh=01_AdTb-BQalZtgH8vuLA-5J6SEvI6KxZ5dv_Lzfgm1A6V1kg&oe=64366909",
     whatssapp: "4195991111", 
-    bio: "Professor de programação nas horas vagas para 2 imbecil.<br><br>já tentou diversos jogos mas nunca foi bom em nenhum, porém é rico e que isso sirva de inspiração a TODES.", 
+    bio: "Professor de programação nas horas vagas para 2 imbecil. já tentou diversos jogos mas nunca foi bom em nenhum, porém é rico e que isso sirva de inspiração a TODES.", 
     subject: "Ciências", 
     cost: "00,00", 
     weekday: [0], 
